e9d8bffa6174128be17453919ad904194c893c16
TaniaMudryk <99959425+TaniaMudryk@users.noreply.github.com>
2023-03-20 21:04:08 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,7 +4,7 @@ import css from './Statistics.module.css';
 export function Statistics({ title, stats }) {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
+      {title ? <h2 className={css.title}>{title}</h2> : ''}
 
       <ul className={css.stat__list}>
         {stats.map(({ id, label, percentage }) => (
